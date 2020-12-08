9b6d73348f3c58485a60b06e7521b55d2f3cb8dc
Crowdbotics <team@crowdbotics.com>
2020-12-08 01:59:16 +0000

    Update components

--- src/features/BlankScreen1183087/screens/blank.js
+++ src/features/BlankScreen1183087/screens/blank.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React from "react"
 import {
   View,
   Image,
@@ -10,32 +10,60 @@ import {
   TextInput,
   StyleSheet,
   ScrollView
-} from "react-native";
-import DateTimePicker from 'react-native-datepicker';
-import Icon from 'react-native-vector-icons/FontAwesome';
-import Slider from '@react-native-community/slider';
-import { CheckBox } from 'react-native-elements';
-import {SlideMenuIcon} from '../../../navigator/slideMenuIcon';
+} from "react-native"
+import DateTimePicker from "react-native-datepicker"
+import Icon from "react-native-vector-icons/FontAwesome"
+import Slider from "@react-native-community/slider"
+import { CheckBox } from "react-native-elements"
+import { SlideMenuIcon } from "../../../navigator/slideMenuIcon"
 
 export default class Blank extends React.Component {
-  static navigationOptions = ({navigation}) => {
+  static navigationOptions = ({ navigation }) => {
     return {
-      headerLeft: <SlideMenuIcon navigationProps={navigation} />,
-    };
-  };
-  
-  state = {};
+      headerLeft: <SlideMenuIcon navigationProps={navigation} />
+    }
+  }
+
+  state = {}
 
   render = () => (
-    <View style={styles.container}>
-      <Text>This is your new component</Text>
+    <View>
+      <View style={styles.View_3}>
+        <View style={styles.View_5}>
+          <Text style={styles.Text_11}>This is page 1</Text>
+          <TouchableOpacity
+            onPress={() => this.props.navigation.navigate("BlankScreen0183086")}
+          >
+            <Image
+              source={{
+                uri:
+                  "https://d3tklmlrp1a8c2.cloudfront.net/media/project_component_resources/plant_7uXgnOV.jpg"
+              }}
+            />
+          </TouchableOpacity>
+          <Text>Press image above to go to page 0</Text>
+          <Text>Press button below to show an alert</Text>
+          <Button title="Press me!" onPress={() => alert("Pressed!")} />
+        </View>
+      </View>
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    marginHorizontal: 16,
+  View_1: {},
+  View_3: {
+    width: "100%",
+    paddingLeft: 20,
+    paddingRight: 20,
+    paddingTop: 20,
+    paddingBottom: 20,
+    backgroundColor: "#8bcdee"
   },
-});
+  View_5: { width: "0100%" },
+  Text_11: { alignSelf: "center" },
+  Image_9: {},
+  Text_13: {},
+  Text_14: {},
+  Button_17: {}
+})
