3abfff6491e1f47c98cf451697dc032ac2c15a7a
Crowdbotics <team@crowdbotics.com>
2020-12-08 01:56:35 +0000

    Update components

--- src/features/BlankScreen0183086/screens/blank.js
+++ src/features/BlankScreen0183086/screens/blank.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React from "react"
 import {
   View,
   Image,
@@ -10,32 +10,41 @@ import {
   TextInput,
   StyleSheet,
   ScrollView
-} from "react-native";
-import DateTimePicker from 'react-native-datepicker';
-import Icon from 'react-native-vector-icons/FontAwesome';
-import Slider from '@react-native-community/slider';
-import { CheckBox } from 'react-native-elements';
-import {SlideMenuIcon} from '../../../navigator/slideMenuIcon';
+} from "react-native"
+import DateTimePicker from "react-native-datepicker"
+import Icon from "react-native-vector-icons/FontAwesome"
+import Slider from "@react-native-community/slider"
+import { CheckBox } from "react-native-elements"
+import { SlideMenuIcon } from "../../../navigator/slideMenuIcon"
 
 export default class Blank extends React.Component {
-  static navigationOptions = ({navigation}) => {
+  static navigationOptions = ({ navigation }) => {
     return {
-      headerLeft: <SlideMenuIcon navigationProps={navigation} />,
-    };
-  };
-  
-  state = {};
+      headerLeft: <SlideMenuIcon navigationProps={navigation} />
+    }
+  }
+
+  state = {}
 
   render = () => (
-    <View style={styles.container}>
-      <Text>This is your new component</Text>
+    <View>
+      <Text style={styles.Text_3}>This is page 0</Text>
+      <TouchableOpacity
+        onPress={() => this.props.navigation.navigate("BlankScreen1183087")}
+      >
+        <Text style={styles.Text_5}>Press this text to go to page 1</Text>
+      </TouchableOpacity>
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    marginHorizontal: 16,
+  View_1: {},
+  Text_3: {
+    fontSize: 26,
+    color: "#1ce718",
+    fontFamily: "CrimsonText-BoldItalic",
+    lineHeight: 26
   },
-});
+  Text_5: { alignSelf: "center" }
+})
