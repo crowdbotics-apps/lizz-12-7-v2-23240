10565e516801727951e0d8ef5303d91d9d615280
lizzb <lizz@crowdbotics.com>
2021-01-06 12:03:28 -0800

    Update screen

--- src/features/BlankScreen4184634/screens/blank.js
+++ src/features/BlankScreen4184634/screens/blank.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React from "react"
 import {
   View,
   Image,
@@ -8,34 +8,52 @@ import {
   Button,
   Switch,
   TextInput,
-  StyleSheet,
-  ScrollView
-} from "react-native";
-import DateTimePicker from 'react-native-datepicker';
-import Icon from 'react-native-vector-icons/FontAwesome';
-import Slider from '@react-native-community/slider';
-import { CheckBox } from 'react-native-elements';
-import {SlideMenuIcon} from '../../../navigator/slideMenuIcon';
-
-export default class Blank extends React.Component {
-  static navigationOptions = ({navigation}) => {
-    return {
-      headerLeft: <SlideMenuIcon navigationProps={navigation} />,
-    };
-  };
-  
-  state = {};
-
+  StyleSheet
+} from "react-native"
+import DateTimePicker from "react-native-datepicker"
+import Icon from "react-native-vector-icons/FontAwesome"
+import Slider from "@react-native-community/slider"
+import { CheckBox } from "react-native-elements"
+import { SlideMenuIcon } from "../../../navigator/slideMenuIcon"
+import { connect } from "react-redux"
+export class Blank extends React.Component {
+  static navigationOptions = ({ navigation }) => {
+    return { headerLeft: <SlideMenuIcon navigationProps={navigation} /> }
+  }
+  state = {}
   render = () => (
-    <View style={styles.container}>
-      <Text>This is your new component</Text>
-    </View>
-  );
+    <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
+      <ImageBackground
+        source={{
+          uri:
+            "https://s3-us-west-2.amazonaws.com/figma-alpha-api/img/e30e/077b/4681dada058860c0876fc3b250982ead"
+        }}
+        style={styles.ImageBackground_1_4}
+      />
+      <View style={styles.View_2} />
+    </ScrollView>
+  )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    marginHorizontal: 16,
+  ScrollView_1: {},
+  ImageBackground_1_4: {
+    width: 414,
+    minWidth: 414,
+    height: 735.3783569335938,
+    minHeight: 735.3783569335938,
+    marginLeft: 0,
+    marginTop: 0,
+    position: "absolute",
+    left: 0,
+    top: 0,
+    resizeMode: "cover"
   },
-});
+  View_2: { height: 736 }
+})
+
+function mapStateToProps(state) {
+  return { state: state.apiReducer }
+}
+const actionCreators = {}
+export default connect(mapStateToProps, actionCreators)(Blank)
